11d6840cf016d8124f9db2cb8084266b09b13346
Dien Cap <diencb@innotech-vn.com>
2023-03-21 10:33:13 +0700

    Analyze and optimize size for build field in production mode

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tsconfigPaths from 'vite-tsconfig-paths'
+import { visualizer } from 'rollup-plugin-visualizer'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), tsconfigPaths()],
+  plugins: [react(), tsconfigPaths(), visualizer()],
   server: {
     port: 3000
   },
