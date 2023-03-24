ec1355c4c5fe1e2a435f257273fda7ed60dcbe66
Dien Cap <diencb@innotech-vn.com>
2023-03-24 17:58:46 +0700

    Update unit test for Profile component

--- src/pages/Profile/Profile.test.tsx
+++ src/pages/Profile/Profile.test.tsx
@@ -2,19 +2,18 @@ import { waitFor } from '@testing-library/react'
 import { describe, it, expect } from 'vitest'
 
 import { path } from '@/constants'
-import { renderWithRouter, setAccessTokenToLS } from '@/utils'
+import { delay, renderWithRouter, setAccessTokenToLS } from '@/utils'
 import { access_token } from '@/msw'
 
 describe('Profile', () => {
-  it('should display profile page', async () => {
+  it('should render UI Profile', async () => {
     setAccessTokenToLS(access_token)
     const { container } = renderWithRouter({ route: path.profile })
 
+    await delay(1000)
     await waitFor(async () => {
       expect(document.querySelector('title')?.textContent).toBe('Profile | Tiki Clone')
-      expect((container.querySelector('form input[placeholder="Thêm tên"]') as HTMLInputElement).value).toBe(
-        'Cap Bao Dien'
-      )
+      expect(container.querySelector('form input[id="name"]')).toBeInTheDocument()
     })
   })
 })
